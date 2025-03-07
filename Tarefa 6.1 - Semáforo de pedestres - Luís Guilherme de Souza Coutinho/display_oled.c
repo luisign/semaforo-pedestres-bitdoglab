7c21591ed48e84b74c32d1549c3d9149bfbdb54c
guilherme <guilhermecoutinho586@gmail.com>
2025-03-07 17:28:11 -0300

    first commit

---
+++ Tarefa 6.1 - Semáforo de pedestres - Luís Guilherme de Souza Coutinho/display_oled.c
@@ -0,0 +1,187 @@
+/*
+Aluno: Luís Guilherme de Souza Coutinho - NAT6
+OBS: Lógica do código feita de acordo com as orientações da publicação 
+"Auxílio e explicação sobre a atividade 6.1" no fórum "Dúvidas e Debates"
+do NAT6, publicado no dia 22. jan. 2025 pelo professor Erick Lima.
+*/
+
+//INCLUSAO DE BIBLIOTECAS:
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "inc/ssd1306.h"
+#include "hardware/i2c.h"
+
+//DEFINICAO DE PINOS (LED RGB, BOTAO A E DISPLAY OLED EM I2C):
+#define LED_R_PIN 13
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+
+#define BTN_A_PIN 5
+
+const uint I2C_SDA = 14;
+const uint I2C_SCL = 15;
+
+//ARMAZENA O ESTADO DO BOTAO:
+int A_state = 0;
+
+//CONFIGURA O SINAL VERDE (ABERTO):
+void SinalAberto(){
+    gpio_put(LED_R_PIN, 0);
+    gpio_put(LED_G_PIN, 1);
+    gpio_put(LED_B_PIN, 0);   
+}
+
+//CONFIGURA O SINAL VERMELHO (FECHADO):
+void SinalFechado(){
+    gpio_put(LED_R_PIN, 1);
+    gpio_put(LED_G_PIN, 0);
+    gpio_put(LED_B_PIN, 0);
+}
+
+//EXIBE A MENSAGEM DO SINAL FECHADO NO DISPLAY:
+void mensagemFechado(){
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    char *text[] = {
+        "SINAL FECHADO",
+        "AGUARDE"};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+}
+
+//EXIBE A MENSAGEM DO SINAL DE ATENCAO NO DISPLAY:
+void mensagemAtencao(){
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    char *text[] = {
+        "ATENCAO",
+        "PREPARE-SE"};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+}
+
+//EXIBE A MENSAGEM DO SINAL ABERTO NO DISPLAY:
+void mensagemAberto(){
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    char *text[] = {
+        "SINAL ABERTO",
+        "ATRAVESSAR COM",
+        "CUIDADO"};
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
+}
+
+//VERIFICA O ESTADO DO BOTAO:
+int WaitWithRead(int timeMS){
+    for(int i = 0; i < timeMS; i = i+100){
+        A_state = !gpio_get(BTN_A_PIN);
+        if(A_state == 1){
+            return 1;
+        }
+        sleep_ms(100);
+    }
+    return 0;
+}
+
+//FUNCAO PRINCIPAL:
+int main()
+{
+    stdio_init_all();
+
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+
+    gpio_init(BTN_A_PIN);
+    gpio_set_dir(BTN_A_PIN, GPIO_IN);
+    gpio_pull_up(BTN_A_PIN);
+
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    ssd1306_init();
+
+    //LOOP PRINCIPAL:
+    while(true) {
+        SinalFechado();
+        mensagemFechado();
+        A_state = WaitWithRead(13000);
+
+        if(A_state){
+            mensagemAtencao();
+            sleep_ms(5000);
+
+            SinalAberto();
+            mensagemAberto();
+            sleep_ms(8000);
+
+        }else{
+            SinalAberto();
+            mensagemAberto();
+            sleep_ms(8000);
+        }
+    }
+    return 0;
+}
